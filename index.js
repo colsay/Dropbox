fe2bcfb44cbcb3924fa8d87fe39ef1a1add221ae
lesleyc@bu.edu <lesleyc@bu.edu>
2020-09-23 17:56:15 +0800

    Still working on template

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+/**********************************************
+ *
+ * ==================================
+ ***********************************************/
+/** #  #
+/*  ====================== */
+/**  */
 const app = require("express")();
+
+// These are the node modules that we installed
 const bodyParser = require("body-parser");
 const expressFileUpload = require("express-fileupload");
+
+// These are the native modules that node already has
+const fs = require("fs");
+const path = require("path");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressFileUpload);
